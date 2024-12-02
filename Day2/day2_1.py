1ef19643cc8b157fb9d17e48def2cc391fdf3195
LiuHaowen <hwen1liu@gmail.com>
2024-12-02 15:21:43 +0800

    day_2_1 add

--- Day2/day2_1.py
+++ Day2/day2_1.py
@@ -1,6 +1,5 @@
 def is_safe(data):
     flag = 1 if data[0] < data[1] else 0   # 1 increase
-    dampener = 0
     if flag == 1:
         for i in range(1, len(data)):
             t = data[i] - data[i - 1]
@@ -17,5 +16,6 @@ def is_safe(data):
 s = 0
 with open('data.txt', 'r') as file:
     for line in file:
-        s += is_safe([int(i) for i in line.split()])
+        data = list(map(int, line.split()))
+        s += is_safe(data)
 print(s)
--- Day2/day2_2.py
+++ Day2/day2_2.py
@@ -1,1 +1,27 @@
-[int(i) for i in line.split()]
+def is_safe(data):
+    flag = 1 if data[0] < data[1] else 0   # 1 increase
+    if flag == 1:
+        for i in range(1, len(data)):
+            t = data[i] - data[i - 1]
+            if not(1 <= t <= 3):
+                return 0
+
+    if flag == 0:
+        for i in range(1, len(data)):
+            t = data[i - 1] - data[i]
+            if not(1 <= t <= 3):
+                return 0
+    return 1
+
+s = 0
+with open('data.txt', 'r') as file:
+    for line in file:
+        data = list(map(int, line.split()))
+        if is_safe(data):
+            s += 1
+        else:
+            for i in range(0, len(data)):
+                if is_safe(data[:i] + data[i + 1:]):
+                    s += 1
+                    break
+print(s)
